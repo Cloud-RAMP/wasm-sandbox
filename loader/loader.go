93c0b14aad93953026847c3252fc9104ca4f0b12
PeterOlsen1 <peterolsen455@gmail.com>
2026-02-17 19:33:11 -0600

    fix: delete unused file

--- loader/loader.go
+++
@@ -1,7 +0,0 @@
-package loader
-
-// load user code and compile it to a WASM binary here
-// Consider supporting multiple languages (AssemblyScript, Rust, Go, whatever compiles to wasm)
-
-// Will probably need to store wasm builds in web file storage
-// Once we load them, we can keep them in memory (maybe a cache with an LRU policy)
